6e68386b69df59c39432d2f89a4565a2fd8da0d6
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 14:55:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,14 +1,11 @@
 def select_winner(passengers)
 passengers = {
  suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
+ suite_b: "Seymour Hoffman", 
  suite_c: "Alfred Tennyson", 
  suite_d: "Charlie Chaplin", 
  suite_e: "Crumpet the Elf"
  }
-
-def select_winner(passengers)
-  # add the code snippet here!
   
   winner = ""
   passengers.each do |suite, name|
