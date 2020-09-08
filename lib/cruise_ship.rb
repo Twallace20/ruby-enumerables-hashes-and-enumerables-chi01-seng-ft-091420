f412554c1828fb30ddd7397c38c3406f378434a4
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 15:16:32 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,11 +6,5 @@ def select_winner(passengers)
  #suite_d: "Charlie Chaplin", 
  #suite_e: "Crumpet the Elf"
  #}#
-  
- winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name 
-end 
+select_winner = [passengers[0]]
 end
-winner
